53746d5bf05355e1470163bc371b1786006a8b68
hong-sile <gurwns9325@naver.com>
2023-10-11 11:57:23 +0900

    cpp 패키지 이동

--- baekJoon/cpp/10844.cpp
+++ baekJoon/cpp/10844.cpp
@@ -1,53 +1,36 @@
 #include<iostream>
- 
-#define endl "\n"
-#define MAX 100 + 1
-#define Moduler 1000000000
-typedef long long ll;
 using namespace std;
- 
-int N;
-ll Answer;
-ll DP[MAX][11];
- 
-void Input(){
-    cin >> N;
-}
- 
-void Solution(){
-    for (int i = 1; i <= 9; i++){
-        DP[1][i] = 1; 
-    }
-    DP[1][0] = 0;
- 
-    for (int i = 2; i <= N; i++)
-    {
-        for (int j = 0; j <= 9; j++)
-        {
-            if (j == 0) DP[i][j] = DP[i - 1][j + 1] % Moduler;
-            else if (j == 9) DP[i][j] = DP[i - 1][j - 1] % Moduler;
-            else DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % Moduler;
-        }
-    }
- 
-    for (int i = 0; i < 10; i++)
-    {
-        Answer = Answer + DP[N][i];
-    }
-    cout << Answer % 1000000000 << endl;
-}
- 
-void Solve(){
-    Input();
-    Solution();
-}
- 
-int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    Solve();
- 
-    return 0;
+
+
+int main(){
+	int n;
+	long long ans=0;
+	long long dp[101][10];
+	
+	dp[1][0]=0;
+	for(int i=1;i<=9;i++){
+		dp[1][i]=1;
+	}
+	
+	cin >> n;
+	
+	//구현부
+	for(int i=2;i<=n;i++){
+		for(int j=0;j<=9;j++){
+			if(j==0)
+				dp[i][j]=dp[i-1][1];
+			else if(j==9)
+				dp[i][j]=dp[i-1][8];
+			else
+				dp[i][j]=(dp[i-1][j-1]+dp[i-1][j+1]) % 1000000000;
+		}
+	}
+	
+	
+	for(int i=0;i<=9;i++){
+		ans+=dp[n][i];
+		ans%=1000000000;
+	}
+	
+	cout << ans;
 }
