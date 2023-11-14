d57b8da293993231138375999e80ecc8030a7a46
hong-sile <gurwns9325@naver.com>
2023-11-14 16:35:25 +0900

    stack 프로그래머스 12909

---
+++ baekJoon/java/stack/Programmers_12909.java
@@ -0,0 +1,28 @@
+package stack;
+
+public class Programmers_12909 {
+
+  public static void main(String[] args) {
+    System.out.println(solution("()()"));
+    System.out.println(solution("(())()"));
+    System.out.println(solution(")()("));
+    System.out.println(solution("(()("));
+  }
+
+  static boolean solution(String s) {
+    int sum = 0;
+
+    for (final char bracket : s.toCharArray()) {
+      if (bracket == '(') {
+        sum += 1;
+      } else if (bracket == ')') {
+        if (sum == 0) {
+          return false;
+        }
+        sum -= 1;
+      }
+    }
+
+    return sum == 0;
+  }
+}
